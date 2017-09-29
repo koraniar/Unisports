7245f986037fd5d964856ec8bef232c65c29ced1
danielito96 <daniel_m9603@hotmail.com>
2017-09-29 09:57:11 -0500

    Team create

--- UnisportsData/src/com/unisports/dao/TeamDAO.java
+++ UnisportsData/src/com/unisports/dao/TeamDAO.java
@@ -7,9 +7,14 @@ package com.unisports.dao;
 
 import com.unisports.database.context.Database;
 import com.unisports.entities.Team;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -18,13 +23,35 @@ import java.util.UUID;
 public class TeamDAO {
     
     private Database _database;
+    private Connection _connection; 
     
     public TeamDAO(){
         _database = new Database();
+        
+        try {
+            _connection = _database.connect();
+        } catch (SQLException ex) {
+            Logger.getLogger(TeamDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     public boolean createTeam(Team team){
-        return true;
+        
+        try {
+            PreparedStatement statement = _connection.prepareStatement("INSERT INTO Team (Id, Name, Logo, Motto, Description, Sport_Id) VALUES (?, ?, ?, ?, ?, ?);");
+            statement.setString(1, team.getId().toString());
+            statement.setString(2, team.getName());
+            statement.setString(3, team.getLogo());
+            
+            int result = statement.executeUpdate();
+            statement.close();
+            //_database.Disconnect();
+            return result > 0;
+        } catch (SQLException ex) {
+            Logger.getLogger(SportDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+        
     }
     
     public boolean updateTeam(Team team){
