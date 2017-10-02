ef632cfe43c4dbe5eb27322425fc5cd51c88933f
Koraniar  <eganfs@gmail.com>
2017-10-02 07:59:10 -0500

    database connection

--- UnisportsData/src/com/unisports/database/context/Database.java
+++ UnisportsData/src/com/unisports/database/context/Database.java
@@ -2,6 +2,7 @@ package com.unisports.database.context;
 
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -11,24 +12,35 @@ public class Database {
     protected static final String user = "root";
     protected static final String password = "12345678";
     protected static final String serverName = "127.0.0.1";
-    protected static final String databaseName = "unisports";
+    protected static final String databaseName = "unisports";    
+    protected static final String databaseType = "mysql";
     protected static final int timeOutTime = 0;
-    private Connection conn;
+    private Connection _connection;
 
     public Connection connect() throws SQLException {
-        MysqlDataSource dataSource = new MysqlDataSource();
-        dataSource.setUser(user);
-        dataSource.setPassword(password);
-        dataSource.setServerName(serverName);
-        dataSource.setDatabaseName(databaseName);
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            
+            _connection = DriverManager.getConnection("jdbc:" + databaseType + "://" + serverName +"/" + databaseName + "?user=" + user + "&password=" + password);            
+            //_connection = DriverManager.getConnection(String.format("jdbc:{0}://{1}/{2}?user={3}&password={4}", databaseType, serverName, databaseName, user, password));
 
-        conn = dataSource.getConnection();
-        return conn;
+        }catch(ClassNotFoundException e){
+            return null;
+        }
+        
+//        MysqlDataSource dataSource = new MysqlDataSource();
+//        dataSource.setUser(user);
+//        dataSource.setPassword(password);
+//        dataSource.setServerName(serverName);
+//        dataSource.setDatabaseName(databaseName);
+//
+//        _connection = dataSource.getConnection();
+        return _connection;
     }
 
     public void Disconnect() {
         try {
-            conn.close();
+            _connection.close();
         } catch (SQLException ex) {
             Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
         }
--- UnisportsTest/src/com/unisports/test/DatabaseTest.java
+++ UnisportsTest/src/com/unisports/test/DatabaseTest.java
@@ -18,9 +18,10 @@ import java.util.UUID;
  */
 public class DatabaseTest {
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
         
-        //Sport();
+        DatabaseTest thisTest =  new DatabaseTest(); 
+        thisTest.Sport();
         
 
     }
@@ -39,27 +40,24 @@ public class DatabaseTest {
         //Get
         Sport sp = sportDao.getSportById(sport.getId());
         if (sp != null) {
-            System.out.println("Entity");
-            System.out.println(sp.getName());            
-            System.out.println(sp.getId());
-            System.out.println(sp.getType());
+            System.out.println("Sport Entity ----\n" + sp.getId() + "\n" + sp.getName() + "\n" + sp.getType() + "\n------------");
         }else{
-            System.out.println("none get by ID");
+            System.out.println("none Sport get by ID");
         }
         
         //Update
-        sp.setName("updated");
+        sp.setName("updated n");
         
         System.out.println("Update " + sportDao.updateSport(sp));        
         
-        System.out.println(sportDao.deleteSport(UUID.fromString("b8c3406b-7268-4f3d-bbe4-848017b90036")));
+        //System.out.println(sportDao.deleteSport(UUID.fromString("b8c3406b-7268-4f3d-bbe4-848017b90036")));
         
         
         //GetAllSports
         List<Sport> sps = sportDao.getAllSports();
         
         if (sps != null && !sps.isEmpty()) {
-            System.out.println("All " + sps.size());
+            System.out.println("All Sports " + sps.size());
         }else{
             System.out.println("none list");
         }
@@ -69,7 +67,7 @@ public class DatabaseTest {
         List<Sport> sps2 = sportDao.getSportsByType(SportType.Basquetball);
         
         if (sps2 != null && !sps2.isEmpty()) {
-            System.out.println("Bas " + sps2.size());
+            System.out.println("Basquet type " + sps2.size());
         }else{
             System.out.println("none list 2");
         }
