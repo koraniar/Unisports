9991e7e820b42678de517c755e70c10c633b4690
Koraniar  <eganfs@gmail.com>
2017-10-06 07:30:51 -0500

    fix conflicts

--- UnisportsData/src/com/unisports/dao/TeamInscriptionDAO.java
+++ UnisportsData/src/com/unisports/dao/TeamInscriptionDAO.java
@@ -23,18 +23,10 @@ import java.util.logging.Logger;
  */
 public class TeamInscriptionDAO {
      private Database _database;
-<<<<<<< HEAD
      private Connection _connection; 
     
     public TeamInscriptionDAO(){
         _database = new Database();
-        
-=======
-     private Connection _connection;
-    
-    public TeamInscriptionDAO(){
-        _database = new Database();
->>>>>>> a15ab44bf081930d625fc11a8c41266c19969d75
         try {
             _connection = _database.connect();
         } catch (SQLException ex) {
@@ -44,17 +36,10 @@ public class TeamInscriptionDAO {
     
     public boolean createTeamInscription(TeamInscription inscription){
         try {
-<<<<<<< HEAD
-            PreparedStatement statement = _connection.prepareStatement("INSERT INTO TeamInscription (Id, Points, EventId) VALUES (?, ?, ?);");
-            statement.setString(1, inscription.getId().toString());
-            statement.setInt(2, inscription.getPoints());
-            statement.setString(3, (inscription.getEventId()).toString());
-=======
             PreparedStatement statement = _connection.prepareStatement("INSERT INTO TeamInscription (Id, Points, Event_Id) VALUES (?, ?, ?);");
             statement.setString(1, inscription.getId().toString());
             statement.setInt(2, inscription.getPoints());
             statement.setString(3, inscription.getEventId().toString());
->>>>>>> a15ab44bf081930d625fc11a8c41266c19969d75
             
             int result = statement.executeUpdate();
             statement.close();
@@ -68,17 +53,10 @@ public class TeamInscriptionDAO {
     
     public boolean updateTeamInscription(TeamInscription inscription){
         try {
-<<<<<<< HEAD
-            PreparedStatement statement = _connection.prepareStatement("UPDATE TeamInscription SET Id = ?, Points = ?, EventId = ? WHERE Id = ?;");
-            statement.setString(1, inscription.getId().toString());
-            statement.setInt(2, inscription.getPoints());
-            statement.setString(3, (inscription.getEventId()).toString());
-=======
             PreparedStatement statement = _connection.prepareStatement("UPDATE TeamInscription SET Points = ?, Event_Id = ? WHERE Id = ?;");
             statement.setInt(1, inscription.getPoints());
             statement.setString(2, inscription.getEventId().toString());
             statement.setString(3, inscription.getId().toString());
->>>>>>> a15ab44bf081930d625fc11a8c41266c19969d75
             
             int result = statement.executeUpdate();
             statement.close();
@@ -95,16 +73,7 @@ public class TeamInscriptionDAO {
             PreparedStatement statement = _connection.prepareStatement("SELECT * FROM TeamInscription;");
             
             ResultSet result = statement.executeQuery();
-            
-<<<<<<< HEAD
-            List<TeamInscription> teamInscriptions = new ArrayList<TeamInscription>();
-            
-            while(result.next()){
-                TeamInscription teamInscription = new TeamInscription(UUID.fromString(result.getString("Id")));
-                teamInscription.setPoints(result.getInt("Subject"));
-                teamInscription.setEventId(UUID.fromString(result.getString("EventId")));
-                teamInscriptions.add(teamInscription);
-=======
+
             List<TeamInscription> inscriptions = new ArrayList<TeamInscription>();
             
             while(result.next()){
@@ -112,28 +81,16 @@ public class TeamInscriptionDAO {
                 tInscription.setPoints(result.getInt("Points"));             
                 tInscription.setEventId(UUID.fromString(result.getString("Event_Id")));
                 inscriptions.add(tInscription);
->>>>>>> a15ab44bf081930d625fc11a8c41266c19969d75
             }
             
             result.close();
             statement.close();
             //_database.Disconnect();
-<<<<<<< HEAD
-            return teamInscriptions;
-=======
             return inscriptions;
->>>>>>> a15ab44bf081930d625fc11a8c41266c19969d75
         } catch (SQLException ex) {
             Logger.getLogger(TeamInscriptionDAO.class.getName()).log(Level.SEVERE, null, ex);
             return null;
         }
-<<<<<<< HEAD
-    }
-    
-    public TeamInscription getTeamInscriptionById(UUID Id){
-        return new TeamInscription();
-=======
->>>>>>> a15ab44bf081930d625fc11a8c41266c19969d75
     }
     
     public TeamInscription getTeamInscriptionById(UUID id){
