28caa58e1ffc322d15d1b199b8e35607294e746a
danielito96 <daniel_m9603@hotmail.com>
2017-10-23 12:41:27 -0500

    View Login

--- UnisportsDesktop/src/unisportsdesktop/UnisportsDesktop.java
+++ UnisportsDesktop/src/unisportsdesktop/UnisportsDesktop.java
@@ -16,13 +16,13 @@ import javafx.stage.Stage;
  * @author danielmontana
  */
 public class UnisportsDesktop extends Application {
-    
+
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+
         Scene scene = new Scene(root);
-        
+
         stage.setScene(scene);
         stage.show();
     }
@@ -33,5 +33,5 @@ public class UnisportsDesktop extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
